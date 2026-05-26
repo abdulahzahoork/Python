03a9fc65f114fc35422c4aa2bdd87e6a5da2ff8b
Muhammad Abdullah Khan <abdulahzahoork@gmail.com>
2026-05-26 13:05:03 +0500

    Practice operators problems 9 to 12

---
+++ Programming Fundamentals/Operators/problem-09.py
@@ -0,0 +1,4 @@
+# Q9. What does a << 1 do to a number mathematically? Verify with a = 6.
+
+a = 6
+print(a << 1)
---
+++ Programming Fundamentals/Operators/problem-12.py
@@ -0,0 +1,5 @@
+# Q12. Given 
+fruits = ["apple", "banana", "mango"]
+#, write a one-liner using a membership operator to check if "grape" is not in the list and print a message accordingly.
+
+print("grape" not in fruits)
