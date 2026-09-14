0395977fefce8aeaa99dce6d06cc22adb9723ccc
Abdullah Khan <abdulahzahoork@gmail.com>
2026-09-14 11:40:06 +0500

    Practice Python basics program 6

---
+++ Educative.io Course/Talk To Machine (Python Basics)/program-06.py
@@ -0,0 +1,21 @@
+# string slicing
+
+
+# reverse a string
+text = "Python"
+reversedText = text[::-1]
+print(reversedText)
+
+
+# Get "Data" from "DataScience"
+
+s = "DataScience"
+print(s[:4])
+
+# Get "Science" from "DataScience"
+
+print(s[4:])
+
+# Get every other character from "DataScience"
+
+print(s[::2])
