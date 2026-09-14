187dd93bd92b0f70eef7a6007e5e683795fc9bd9
Abdullah Khan <abdulahzahoork@gmail.com>
2026-09-14 11:09:14 +0500

    Practice Python Basics program 2

---
+++ Educative.io Course/Talk To Machine (Python Basics)/program-02.py
@@ -0,0 +1,18 @@
+#Python Arithmetic Operators: Add, Subtract, Multiply, Divide
+
+
+print(4+7) #addition
+
+print(10-5) #subtraction
+
+print(10*5) #multiplication
+
+print(10/2) #float division
+
+print(10//2)  #floor division
+
+print(10%2) #modulus
+
+print(2 + 3 * 5) #17, not 25 (BODMAS rule)
+
+print((2+3) * 5) #25 (BODMAS rule)
