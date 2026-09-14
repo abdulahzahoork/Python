e30b0363031e88c8d58f686f703ccf7c0188f96a
Abdullah Khan <abdulahzahoork@gmail.com>
2026-09-14 11:54:29 +0500

    practice python basics program 8

---
+++ Educative.io Course/Talk To Machine (Python Basics)/program-08.py
@@ -0,0 +1,9 @@
+# Python input() Function: Get User Input and Convert Types
+
+# Input user's name
+
+name = input("Enter your name: ")
+print(f"Hello, {name}")
+
+age = int(input("Enter your age: "))
+print(f"In 5 years, your age will be {age+5}.")
