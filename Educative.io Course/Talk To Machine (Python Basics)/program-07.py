e1b1eeb50d84d04822b0fd259228c4085afd65b6
Abdullah Khan <abdulahzahoork@gmail.com>
2026-09-14 11:46:30 +0500

    Practice python basics program 7

---
+++ Educative.io Course/Talk To Machine (Python Basics)/program-07.py
@@ -0,0 +1,15 @@
+# Create variables for your character’s:
+
+# name (text)
+
+# species (text)
+
+# age (number)
+
+# Use print() to introduce your character in one or two sentences
+
+name = "Abdullah"
+specie = "Homo-Sapien"
+age = 21
+
+print(f"Meet {name}! A {age}-year-old {specie}.")
