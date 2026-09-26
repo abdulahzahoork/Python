bc019fa5f23125662c666a1d9e4ecc57b45b2896
Abdullah Khan <abdulahzahoork@gmail.com>
2026-09-26 12:54:49 +0500

    Task-02.py

---
+++ University Lab Tasks/Task-02.py
@@ -0,0 +1,14 @@
+# Variables, Strings and inputs
+
+v = 1
+print("Value of v is now:", v)
+v += 1
+print(f"And now is: {v}")
+
+word1 = "Good"
+word2 = "Morning"
+sentence = word1 + " " + word2
+print(sentence)
+
+name = input("What's your name?\n")
+print(name, "... nice to meet you!")
